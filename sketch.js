a32080685f012d650ff9d6b45af1674ca005fd7a
iasmim2905 <162471102+iasmim2905@users.noreply.github.com>
2024-10-17 11:57:46 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,55 @@
+// fantasia, aventura, drama
+
+// a viagem de chihiro, LIVRE, fantasia, aventura
+// paddington, LIVRE, fantasia, aventura
+// as aventuras de pi, 10, drama, fantasia, aventura
+// depois da chuva, 10, drama
+// Tinker Bell, 16, fantasia, aventura
+// O Filho Protegido, 12, drama
+// fala serio mãe,14 , drama 
+//idade >= 10
+
+ let campoIdade;
+let campoFantasia;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("18");
+  campoFantasia = createCheckbox("Gosta de drama?");
 }
 
+
 function draw() {
-  background(220);
+    background("rgb(223,18,18)");
+    let idade = campoIdade.value();
+    let gostaDeFantasia = campoFantasia.checked();
+    let recomendacao = geraRecomendacao(idade, gostaDeFantasia);
+
+    fill(color(12, 0, 15));
+    textAlign(CENTER, CENTER);
+    textSize(38);
+    text(recomendacao, width / 2, height / 2);
 }
+
+function geraRecomendacao(idade, gostaDeFantasia) {
+    if (idade >= 10) {
+        if (idade >= 14) {
+            return "O Filho Protegido";
+        } else {
+            if (gostaDeFantasia) {
+                return "fala serio mãe";
+            } else {
+                return "Tinker Bell";
+            }
+        }
+    } else {
+        if (gostaDeFantasia) {
+            return "A viagem de chihiro";
+        } else {
+            return "O feitiço do tempo";
+        }
+    }
+}
+
